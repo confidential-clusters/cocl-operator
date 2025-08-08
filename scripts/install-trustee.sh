2ad8bf5fe895a06f1d2672ea2749a8f713b88946
Jakob Naucke <jnaucke@redhat.com>
2025-08-08 16:30:28 +0200

    scripts: Patch Trustee with custom images
    To be reverted once everything is upstream.

--- scripts/install-trustee.sh
+++ scripts/install-trustee.sh
@@ -26,6 +26,35 @@ spec:
   sourceNamespace: olm
 EOF
 
+operator=trustee-operator.v0.4.0
+kubectl_do_csv() {
+	verb=$1
+	shift
+	kubectl "$verb" -n "$namespace" csv "$operator" "$@"
+}
+
+kubectl_get_csv_env_name() {
+	kubectl_do_csv get \
+		-o jsonpath="{.spec.install.spec.deployments[0].spec.template.spec.containers[0].env[$1].name}"
+}
+
+kubectl_patch_csv_env_value() {
+	kubectl_do_csv patch --type="json" \
+		-p="[{'op': 'replace', 'path': '/spec/install/spec/deployments/0/spec/template/spec/containers/0/env/$1/value', 'value':$2}]"
+}
+
+kubectl_do_csv wait --for=create --timeout=90s
+kubectl_do_csv wait --for=jsonpath="{.status.phase}"=Succeeded
+
+if [ "$(kubectl_get_csv_env_name 2)" != "KBS_IMAGE_NAME_MICROSERVICES" ] || \
+	[ "$(kubectl_get_csv_env_name 3)" != "AS_IMAGE_NAME" ]; then
+	echo "Unexpected change in Trustee CSV environment order"
+	exit 1
+fi
+
+kubectl_patch_csv_env_value 2 "quay.io/afrosi_rh/kbs-grpc-as:latest"
+kubectl_patch_csv_env_value 3 "quay.io/afrosi_rh/coco-as-grpc:latest"
+
 kubectl apply -f - << EOF
 apiVersion: v1
 kind: Service
