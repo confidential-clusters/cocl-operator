9d4cf1e85d02a9dedd7c7cb20b5dbfb9c2ed4237
Jakob Naucke <jnaucke@redhat.com>
2025-08-08 15:17:29 +0200

    kind: KBS port pass-through to host

--- scripts/install-trustee.sh
+++ scripts/install-trustee.sh
@@ -11,15 +11,33 @@ curl -sL \
 	https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v${OP_FRMWK_VERSION}/install.sh | \
 	bash -s v${OP_FRMWK_VERSION}
 
+namespace=operators
+
 kubectl apply -f - << EOF
 apiVersion: operators.coreos.com/v1alpha1
 kind: Subscription
 metadata:
   name: my-trustee-operator
-  namespace: operators
+  namespace: ${namespace}
 spec:
   channel: alpha
   name: trustee-operator
   source: operatorhubio-catalog
   sourceNamespace: olm
+EOF
+
+kubectl apply -f - << EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: kbs-forward
+  namespace: ${namespace}
+spec:
+  type: NodePort
+  ports:
+  - name: http
+    nodePort: 31000
+    port: 8080
+  selector:
+    app: kbs
 EOF
